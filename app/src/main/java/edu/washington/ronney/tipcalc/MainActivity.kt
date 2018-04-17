e3eaa97a121537db80330df83e7268009d04af68
ronney221 <ronneydo1@gmail.com>
2018-04-16 23:24:31 -0700

    extra credit

--- app/src/main/java/edu/washington/ronney/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/ronney/tipcalc/MainActivity.kt
@@ -11,9 +11,15 @@ import android.widget.Toast
 import java.math.BigDecimal
 import android.widget.*
 import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.view.*
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    var tipValue : Double = 0.15
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         val input : EditText = findViewById<View>(R.id.editText) as EditText
         val button = findViewById<View>(R.id.button) as Button
+        val seekBar = findViewById<View>(R.id.seekBar) as SeekBar
+
         button.setEnabled(false)
 
         button.setOnClickListener() {
             var tip = input.getText().toString()
             tip = tip.replace("$", "")
-            var newTip = BigDecimal(tip.toDouble() * 0.15).
+            var newTip = BigDecimal(tip.toDouble() * tipValue).
                     setScale(2, BigDecimal.ROUND_HALF_UP).toDouble()
 
             Toast.makeText(this@MainActivity, "$" + newTip.toString(), Toast.LENGTH_SHORT).show()
@@ -77,5 +85,20 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+
+       seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                tipValue = (p1+10).toDouble() / 100
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
     }
 }
+
